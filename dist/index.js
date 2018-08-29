75db4231f2434159aa69fb8c67a69adf3b116d97
Reiniel Herrera <reiniel.herrera@nyumc.org>
2018-08-28 23:40:20 -0500

    Upgrade version of babel and include compilation for ES2015

--- dist/index.js
+++ dist/index.js
@@ -1,190 +1,241 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+'use strict';
 
-class CountDown extends React.Component {
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      const date = this.getDateData(this.props.date);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var CountDown = function (_React$Component) {
+  _inherits(CountDown, _React$Component);
+
+  function CountDown() {
+    _classCallCheck(this, CountDown);
+
+    return _possibleConstructorReturn(this, (CountDown.__proto__ || Object.getPrototypeOf(CountDown)).apply(this, arguments));
+  }
+
+  _createClass(CountDown, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.interval = setInterval(function () {
+        var date = _this2.getDateData(_this2.props.date);
+        if (date) {
+          _this2.setState(date);
+        } else {
+          _this2.stop();
+          _this2.props.onEnd();
+        }
+      }, 1000);
+    }
+  }, {
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      var date = this.getDateData(this.props.date);
       if (date) {
         this.setState(date);
-      } else {
-        this.stop();
-        this.props.onEnd();
       }
-    }, 1000);
-  }
-  componentWillMount() {
-    const date = this.getDateData(this.props.date);
-    if (date) {
-      this.setState(date);
     }
-  }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      this.stop();
+    }
+  }, {
+    key: 'getDateData',
+    value: function getDateData(endDate) {
+      var diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
 
-  componentWillUnmount() {
-    this.stop();
-  }
-  getDateData(endDate) {
-    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+      if (diff <= 0) {
+        return false;
+      }
 
-    if (diff <= 0) {
-      return false;
-    }
+      var timeLeft = {
+        years: 0,
+        days: 0,
+        hours: 0,
+        min: 0,
+        sec: 0,
+        millisec: 0
+      };
 
-    const timeLeft = {
-      years: 0,
-      days: 0,
-      hours: 0,
-      min: 0,
-      sec: 0,
-      millisec: 0
-    };
-
-    if (diff >= 365.25 * 86400) {
-      timeLeft.years = Math.floor(diff / (365.25 * 86400));
-      diff -= timeLeft.years * 365.25 * 86400;
+      if (diff >= 365.25 * 86400) {
+        timeLeft.years = Math.floor(diff / (365.25 * 86400));
+        diff -= timeLeft.years * 365.25 * 86400;
+      }
+      if (diff >= 86400) {
+        timeLeft.days = Math.floor(diff / 86400);
+        diff -= timeLeft.days * 86400;
+      }
+      if (diff >= 3600) {
+        timeLeft.hours = Math.floor(diff / 3600);
+        diff -= timeLeft.hours * 3600;
+      }
+      if (diff >= 60) {
+        timeLeft.min = Math.floor(diff / 60);
+        diff -= timeLeft.min * 60;
+      }
+      timeLeft.sec = diff;
+      return timeLeft;
     }
-    if (diff >= 86400) {
-      timeLeft.days = Math.floor(diff / 86400);
-      diff -= timeLeft.days * 86400;
-    }
-    if (diff >= 3600) {
-      timeLeft.hours = Math.floor(diff / 3600);
-      diff -= timeLeft.hours * 3600;
-    }
-    if (diff >= 60) {
-      timeLeft.min = Math.floor(diff / 60);
-      diff -= timeLeft.min * 60;
-    }
-    timeLeft.sec = diff;
-    return timeLeft;
-  }
-
-  getSeparator() {
-    return React.createElement(
-      'div',
-      { className: 'countdown-separator' },
-      React.createElement(
-        'span',
-        null,
-        ':'
-      )
-    );
-  }
-
-  render() {
-    const countDown = this.state;
-    let days;
-    if (countDown.days === 1) {
-      days = this.props.days.singular;
-    } else {
-      days = this.props.days.plural;
-    }
-    return React.createElement(
-      'div',
-      { className: this.props.className },
-      countDown.days > 0 && React.createElement(
+  }, {
+    key: 'getSeparator',
+    value: function getSeparator() {
+      return _react2.default.createElement(
         'div',
-        { className: `${this.props.className}-col is-day` },
-        React.createElement(
-          'p',
+        { className: 'countdown-separator' },
+        _react2.default.createElement(
+          'span',
           null,
-          React.createElement(
-            'strong',
+          ':'
+        )
+      );
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var countDown = this.state;
+      var days = void 0;
+      if (countDown.days === 1) {
+        days = this.props.days.singular;
+      } else {
+        days = this.props.days.plural;
+      }
+      return _react2.default.createElement(
+        'div',
+        { className: this.props.className },
+        countDown.days > 0 && _react2.default.createElement(
+          'div',
+          { className: this.props.className + '-col is-day' },
+          _react2.default.createElement(
+            'p',
             null,
-            this.leadingZeros(countDown.days)
+            _react2.default.createElement(
+              'strong',
+              null,
+              this.leadingZeros(countDown.days)
+            )
+          ),
+          _react2.default.createElement(
+            'span',
+            { className: 'countdown-time-labels' },
+            days
           )
         ),
-        React.createElement(
-          'span',
-          { className: 'countdown-time-labels' },
-          days
-        )
-      ),
-      this.getSeparator(),
-      React.createElement(
-        'div',
-        { className: `${this.props.className}-col is-hour` },
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'strong',
+        this.getSeparator(),
+        _react2.default.createElement(
+          'div',
+          { className: this.props.className + '-col is-hour' },
+          _react2.default.createElement(
+            'p',
             null,
-            this.leadingZeros(countDown.hours)
+            _react2.default.createElement(
+              'strong',
+              null,
+              this.leadingZeros(countDown.hours)
+            )
+          ),
+          _react2.default.createElement(
+            'span',
+            { className: 'countdown-time-labels' },
+            this.props.hours
           )
         ),
-        React.createElement(
-          'span',
-          { className: 'countdown-time-labels' },
-          this.props.hours
-        )
-      ),
-      this.getSeparator(),
-      React.createElement(
-        'div',
-        { className: `${this.props.className}-col is-min` },
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'strong',
+        this.getSeparator(),
+        _react2.default.createElement(
+          'div',
+          { className: this.props.className + '-col is-min' },
+          _react2.default.createElement(
+            'p',
             null,
-            this.leadingZeros(countDown.min)
+            _react2.default.createElement(
+              'strong',
+              null,
+              this.leadingZeros(countDown.min)
+            )
+          ),
+          _react2.default.createElement(
+            'span',
+            { className: 'countdown-time-labels' },
+            this.props.mins
           )
         ),
-        React.createElement(
-          'span',
-          { className: 'countdown-time-labels' },
-          this.props.mins
-        )
-      ),
-      this.getSeparator(),
-      React.createElement(
-        'div',
-        { className: `${this.props.className}-col is-seg` },
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'strong',
+        this.getSeparator(),
+        _react2.default.createElement(
+          'div',
+          { className: this.props.className + '-col is-seg' },
+          _react2.default.createElement(
+            'p',
             null,
-            this.leadingZeros(countDown.sec)
+            _react2.default.createElement(
+              'strong',
+              null,
+              this.leadingZeros(countDown.sec)
+            )
+          ),
+          _react2.default.createElement(
+            'span',
+            { className: 'countdown-time-labels' },
+            this.props.segs
           )
-        ),
-        React.createElement(
-          'span',
-          { className: 'countdown-time-labels' },
-          this.props.segs
         )
-      )
-    );
-  }
-  stop() {
-    clearInterval(this.interval);
-  }
-  leadingZeros(num, length = null) {
+      );
+    }
+  }, {
+    key: 'stop',
+    value: function stop() {
+      clearInterval(this.interval);
+    }
+  }, {
+    key: 'leadingZeros',
+    value: function leadingZeros(num) {
+      var length = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
 
-    let length_ = length;
-    let num_ = num;
-    if (length_ === null) {
-      length_ = 2;
+
+      var length_ = length;
+      var num_ = num;
+      if (length_ === null) {
+        length_ = 2;
+      }
+      num_ = String(num_);
+      while (num_.length < length_) {
+        num_ = '0' + num_;
+      }
+      return num_;
     }
-    num_ = String(num_);
-    while (num_.length < length_) {
-      num_ = '0' + num_;
-    }
-    return num_;
-  }
-};
+  }]);
+
+  return CountDown;
+}(_react2.default.Component);
+
+;
 
 CountDown.propTypes = {
-  date: PropTypes.string,
-  className: PropTypes.string,
-  days: PropTypes.objectOf(PropTypes.string),
-  hours: PropTypes.string,
-  mins: PropTypes.string,
-  segs: PropTypes.string,
-  onEnd: PropTypes.func
+  date: _propTypes2.default.string,
+  className: _propTypes2.default.string,
+  days: _propTypes2.default.objectOf(_propTypes2.default.string),
+  hours: _propTypes2.default.string,
+  mins: _propTypes2.default.string,
+  segs: _propTypes2.default.string,
+  onEnd: _propTypes2.default.func
 };
 
 CountDown.defaultProps = {
@@ -197,7 +248,7 @@ CountDown.defaultProps = {
   hours: 'Hours',
   mins: 'Min',
   segs: 'Seg',
-  onEnd: () => {}
+  onEnd: function onEnd() {}
 };
 
 CountDown.state = {
@@ -208,4 +259,4 @@ CountDown.state = {
 };
 CountDown.displayName = 'Simple countDown';
 
-export default CountDown;
+exports.default = CountDown;
